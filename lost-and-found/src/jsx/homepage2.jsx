370fb6bd3867ff4b13030ae7aba66b32d3809e93
YenTeze <marklorenzuy25@gmail.com>
2024-10-21 04:52:16 +0800

    CHEER UP

--- lost-and-found/src/jsx/homepage2.jsx
+++ lost-and-found/src/jsx/homepage2.jsx
@@ -5,6 +5,10 @@ import img1 from "../assets/info-1-1.png";
 import img2 from "../assets/info-2-2.png";
 import Report1_Img from "../assets/Report1_Img.png";
 import Report2_Img from "../assets/Report2_Img.png";
+import Memo1Img from "../assets/Memo1Img.png";
+import Memo2Img from "../assets/Memo2Img.png";
+import Report1Img from "../assets/Report1Img.png";
+import Report2Img from "../assets/Report2Img.png";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
@@ -68,120 +72,94 @@ function Homepage2() {
 
         <img src={notif} alt="NU Logo" className="notif" />
       </div>
+
       <div className="sections">
-        <div id="body1" className="body1">
-          <div className="paragraphs">
-            <h1 id="h1">The lost items are in DO’s hands.</h1>
-            <p id="h2">
+        <div className="HomePage">
+
+          <div className="HomePageContent">
+            <h1>The lost items are in DO’s hands.</h1>
+            <p>
               Welcome to our page, the easy way to manage lost and found items
               on campus. Quickly report and locate missing belongings, helping
               students reconnect with their items.
             </p>
           </div>
-          <hr />
-          <div className="infocount">
-            <div className="lostitems">
-              <h1 id="lostitemcount">99+</h1>
-              <p>Lost Items</p>
+          <div className="ItemStatus">
+            <div className="LostStatus">
+              <h2>99+</h2>
+              <span>Lost Items</span>
             </div>
-            <div className="founditems">
-              <h1 id="founditemscount">99+</h1>
+            <div className="FoundStatus">
+              <h2>98</h2>
               <p>Found Items</p>
             </div>
           </div>
         </div>
 
-        <div id="body2" className="body2">
-          <img src={img1} alt="NU Logo" className="large" />
-          <div className="paragraphs2">
-            <h1 id="h3">Memorandum for the Disposal of Found Items.</h1>
-            <ul className="h4">
-              <li>
-                Unclaimed property that easily decays, releases odor, or is
-                perishable will be disposed of within 48 hours. Proper
-                documentation, such as a picture, will be provided.
-              </li>
-              <li>
-                Unclaimed non-perishable property will be disposed of after the
-                end of the academic year.
-              </li>
-              <li>
-                All items shredded or disposed of must be recorded in the Lost
-                and Found Property Logbook.
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div id="body2" className="body2">
-          <div className="paragraphs2" id="paragraphs2">
-            <h1 id="h3">Memorandum for the Disposal of Found Items.</h1>
-            <ul className="h4">
-              <li>
-                Perishable and personal items that can emit foul odor must be
-                claimed within 48 hours to prevent pest infestation.
-              </li>
-              <li>
-                Non-perishable items can be claimed at the end of the term.
-              </li>
-            </ul>
-            <p className="h4">
-              Items that are perishable and other personal items that can emit
-              foul odor are as follows.
-            </p>
-            <ul className="h4">
-              <li>Food and Beverages (Lunch Box, Tumbler, etc.)</li>
-              <li>Personal Care Items (Toiletries, etc.)</li>
-              <li>Fabric (Clothes, Lab Gown, Towel, Jacket Socks, etc.)</li>
-            </ul>
-            <p className="h4">Items that are non-perishable are as follows.</p>
-            <ul className="h4">
-              <li>Accessories</li>
-              <li>Electronics</li>
-            </ul>
-          </div>
-          <img src={img2} alt="NU Logo" className="large" id="large2" />
-        </div>
-
-        <div id="body4" className="body3">
-          <img src={Report1_Img} alt="NU Logo" className="large" />
-          <div className="paragraphs2">
-            <h1 className="text" id="h5">
-              Report an Item that you Found.
-            </h1>
+        <div className="Memo1">
+          <img src={Memo1Img} className="Memo1Img" />
+          <div className="Memo1TextContainer">
+            <h1>Memorandum for the Disposal of Found Items.</h1>
             <p>
-              When reporting a found item, please follow the necessary steps
-              below to help us identify the item and the person who surrendered
-              it.
+              • Unclaimed property that easily decays, releases odor, or is perishable will be disposed of within 48 hours.
+              Proper documentation, such as a picture, will be provided.<br />
+              • Unclaimed non-perishable property will be disposed of after the end of the academic year.<br />
+              • All items shredded or disposed of must be recorded in the Lost and Found Property Logbook.
             </p>
-            <ul className="h4" id="text">
-              <li>Please read the Terms and Conditions.</li>
-              <li>Describe the item you found.</li>
-              <li>Fill out the Response Form.</li>
-            </ul>
-            <button className="ReportFoundbtn"onClick={GoToReportFoundItem} >Report a Found Item</button>
           </div>
         </div>
 
-        <div id="body3" className="body3">
-          <div className="paragraphs5">
-            <h1 className="text" id="h5">
-              Report an Item that you Lost.
-            </h1>
+        <div className="Memo2">
+          <div className="Memo2TextContainer">
+            <h1>Memorandum for the Claiming of Found Items.</h1>
             <p>
-              When reporting a missing item, please follow the necessary steps
-              below to help us identify you and check for matching items based
-              on your description.
+              • Perishable and personal items that can emit foul odor must be claimed within 48 hours to prevent pest infestation.<br />
+              • Non-perishable items can be claimed at the end of the term.<br />
+              <br />
+              Items that are perishable and other personal items that can emit foul odor are as follows.<br />
+              • Food and Beverages (Lunch Box, Tumbler, etc.)<br />
+              • Personal Care Items (Toiletries, etc.)<br />
+              • Fabric (Clothes, Lab Gown, Towel, Jacket Socks, etc.)<br />
+              <br />
+              Items that are non-perishable are as follows.<br />
+              • Accessories<br />
+              • Electronics
             </p>
-            <ul className="h4" id="text">
-              <li>Please read the Terms and Conditions.</li>
-              <li>Describe the item you lost.</li>
-              <li>Fill out the Response Form.</li>
-            </ul>
+          </div>
+          <img src={Memo2Img} className="Memo2Img" />
+        </div>
+
+
+        <div className="Report1">
+          <img src={Report1Img} className="Report1Img" />
+          <div className="Report1TextContainer">
+            <h1>Report a Found Item.</h1>
+            <p>
+              When reporting a found item, please follow the necessary steps below to help us identify the item and the person who surrendered it.<br />
+              <br />
+              • Please read the Terms and Conditions.<br />
+              • Describe the item you found.<br />
+              • Fill out the Response Form.
+            </p>
+            <button className="ReportFoundbtn" onClick={GoToReportFoundItem} >Report a Found Item</button>
+          </div>
+        </div>
+
+        <div className="Report2">
+          <div className="Report2TextContainer">
+            <h1>Report a Missing Item.</h1>
+            <p>
+              When reporting a missing item, please follow the necessary steps below to help us identify you and check for matching items based on your description.<br />
+              <br />
+              • Please read the Terms and Conditions.<br />
+              • Describe the item you lost.<br />
+              • Fill out the Response Form.
+            </p>
             <button className="ReportLostbtn" onClick={GoToReportLostItem}>
               Report a Missing Item
             </button>
           </div>
-          <img src={Report2_Img} alt="NU Logo" className="large" id="large2" />
+          <img src={Report2Img} className="Report2Img" />
         </div>
       </div>
     </div>
